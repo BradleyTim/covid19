4cf4cefa14590f8d7a258ea03dae70e5fa00e191
BradleyTim <timbradley94@gmail.com>
2020-03-16 15:23:23 +0300

    css margin and padding reset

--- src/App.vue
+++ src/App.vue
@@ -22,15 +22,15 @@ export default {
 
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-
-  /* min-height: 60vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between; */
 }
 </style>
