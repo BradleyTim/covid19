d8bd1161ede47d0e0d6294e89d90a093252b17ad
BradleyTim <timbradley94@gmail.com>
2020-03-21 18:40:18 +0300

    change transition to slide-fade animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header></Header>
-    <transition name="fade">
+    <transition name="slide-fade" mode="out-in">
       <router-view :key="$route.path"/>
     </transition>
     <Footer></Footer>
@@ -88,11 +88,25 @@ export default {
   width: 125px;
 }
 
-.fade-enter-active, .fade-leave-active {
+/* .fade-enter-active, .fade-leave-active {
   transition: opacity .4s;
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
-}
+} */
 
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
